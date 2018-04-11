aa6f7a9b8628d421c4cfc2f9711c0de51a4f04ca
Abigail Gray <Abigail Gray>
2018-04-11 11:12:02 -0700

    updates

--- src/filter_image.c
+++ src/filter_image.c
@@ -8,25 +8,93 @@
 
 void l1_normalize(image im)
 {
-    // TODO
+   int pix;
+   int sum = 0;
+
+   for(pix = 0; pix < im.w * im.h * im.c; pix++) {
+       sum += im.data[pix];
+   }
+
+    for(pix = 0; pix < im.w * im.h * im.c; pix++) {
+       im.data[pix] = im.data[pix]/sum;
+    }
 }
 
 image make_box_filter(int w)
 {
-    // TODO
-    return make_image(1,1,1);
+    image filter = make_image(w, w, 1);
+
+    int pix;
+    for(pix = 0; pix < w * w; pix++) {
+        filter.data[pix] = 1;
+    }
+
+    l1_normalize(filter);
+
+    return filter;
 }
 
 image convolve_image(image im, image filter, int preserve)
 {
-    // TODO
-    return make_image(1,1,1);
+    // check filter is valid
+    assert(filter.c == im.c || filter.c == 1);
+    image f;
+
+    if(preserve) {
+        // want same number of channels
+        f = make_image(im.w, im.h, im.c);
+    } else {
+        f = make_image(im.w, im.h, 1);
+    }
+
+    // iterate over all pixels in im
+        // iterate over all in filter to add to sum
+        //set pixel in im
+
+    int channel, col, row, frow, fcol;
+    int filter_sz = filter.w * filter.h;
+
+    for(channel = 0; channel < im.c; channel++) {
+        int f_c = channel;
+        if(filter.c == 1) {
+            f_c = 0;
+        }
+
+        for(row = 0; row < im.h; row++) {
+            for(col = 0; col < im.w; col++) {
+                float q = 0;
+
+                for(frow = 0; frow < filter.h; frow++) {
+                    for(fcol = 0; fcol < filter.w; fcol++) {
+                        q += get_pixel(filter, fcol, frow, f_c) * get_pixel(im, col - (filter.w - 1 - fcol), row - (filter.h - 1 - frow), channel);
+                    }
+                }
+
+                if(preserve) {
+                    set_pixel(f, col, row, channel, q);
+                } else {
+                    float val = get_pixel(f, col, row, 0);
+                    set_pixel(f, col, row, 0, q + val);
+                }
+            }
+            col = 0;
+        }
+        row = 0;
+    }
+    
+    return f;
 }
 
 image make_highpass_filter()
 {
-    // TODO
-    return make_image(1,1,1);
+    image filter = make_image(3, 3, 1);
+    set_pixel(filter, 1, 0, 0, -1);
+    set_pixel(filter, 0, 1, 0, -1);
+    set_pixel(filter, 2, 1, 0, -1);
+    set_pixel(filter, 1, 2, 0, -1);
+    set_pixel(filter, 1, 1, 0, 4);
+
+    return filter;
 }
 
 image make_sharpen_filter()
--- src/resize_image.c
+++ src/resize_image.c
@@ -1,27 +1,118 @@
 #include <math.h>
+#include <stdio.h>
 #include "image.h"
 
 float nn_interpolate(image im, float x, float y, int c)
 {
-    // TODO Fill in
-    return 0;
+    return get_pixel(im, round(x), round(y), c);
 }
 
 image nn_resize(image im, int w, int h)
 {
-    // TODO Fill in (also fix that first line)
-    return make_image(1,1,1);
+    image resize = make_image(w, h, im.c);
+
+    float a = (-0.5 - (im.w-0.5))/(-0.5 - (w - 0.5));
+    float b = -0.5 + 0.5 * a;
+
+    float a_y = (-0.5 - (im.h-0.5))/(-0.5 - (h - 0.5));
+    float b_y = -0.5 + 0.5 * a_y;
+
+    int row;
+    int col;
+
+    for (row = 0; row < resize.h; row++){
+        for(col = 0; col < resize.w; col++) {
+            float c_pos = a * col + b;
+            float r_pos = a_y * row + b_y;
+
+            float red = nn_interpolate(im, c_pos, r_pos, 0);
+            set_pixel(resize, col, row, 0, red);
+
+            float green = nn_interpolate(im, c_pos, r_pos, 1);
+            set_pixel(resize, col, row, 1, green);
+
+            float blue = nn_interpolate(im, c_pos, r_pos, 2);
+            set_pixel(resize, col, row, 2, blue);
+       }
+       col = 0;
+    }
+
+
+    return resize;
+}
+
+
+int round_down(float x) {
+
+    return (int) x;
+}
+
+int round_up(float x) {
+    return (int) x + 1;
 }
 
 float bilinear_interpolate(image im, float x, float y, int c)
 {
-    // TODO
-    return 0;
+    int left_x = round_down(x); 
+    int right_x = round_up(x);
+    int top_y = round_down(y);
+    int bottom_y = round_up(y);
+
+    // printf("%f, %f\n", x, y);
+    // printf("x: %f % f\n", left_x, right_x);
+    // printf("y: %f %f\n", up_y, down_y);
+
+    float v1 = get_pixel(im, left_x, top_y, c);
+    float v2 = get_pixel(im, right_x, top_y, c);
+    float v3 = get_pixel(im, left_x, bottom_y, c);
+    float v4 = get_pixel(im, right_x, bottom_y, c);
+
+    float d1 = x - left_x;
+    float d2 = right_x - x;
+    float d3 = y - top_y;
+    float d4 = bottom_y - y;
+
+    float q1 = v1 * d2 + v2 * d1;
+    float q2 = v3 * d2 + v4 * d1;
+    float q = q1 * d4 + q2 * d3;
+
+    return q;
 }
 
 image bilinear_resize(image im, int w, int h)
 {
-    // TODO
-    return make_image(1,1,1);
+   image resize = make_image(w, h, im.c);
+
+    float a_x = (-0.5 - (im.w-0.5))/(-0.5 - (w * 1.0 - 0.5));
+    float b_x = -0.5 + 0.5 * a_x;
+
+    float a_y = (-0.5 - (im.h-0.5))/(-0.5 - (h * 1.0 - 0.5));
+    float b_y = -0.5 + 0.5 * a_y;
+
+    int row;
+    int col;
+
+    for (row = 0; row < h; row++){
+        for(col = 0; col < w; col++) {
+            float xold = a_x * col + b_x;
+            float yold = a_y * row + b_y;
+
+            float red = bilinear_interpolate(im, xold, yold, 0);
+            set_pixel(resize, col, row, 0, red);
+
+            float green = bilinear_interpolate(im, xold, yold, 1);
+            set_pixel(resize, col, row, 1, green);
+
+            float blue = bilinear_interpolate(im, xold, yold, 2);
+            set_pixel(resize, col, row, 2, blue);
+       }
+
+       col = 0;
+    }
+
+
+    return resize;
 }
+
+
 
